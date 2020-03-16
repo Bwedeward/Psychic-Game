89e80d8140145870997cd50804804c2f734e4f8f
Brooke W <bwedeward@gmail.com>
2020-03-16 16:46:44 -0500

    added javascript to html

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,9 +1,45 @@
-document.onkeyup = function(event) {
-  //var computerOptions = ["abcdefghijklmnopqrstuvwxyz"];
-  //var computerChoices = computerOptions.split(" ");
-  var computerChoices = ["r", "p", "s"];
+var wins = 0;
+var lose = 0;
+var numGuesses = 9;
+var guessed = [];
 
-  // Determines which key was pressed.
+var winsText = document.getElementById("wins-text");
+var lossesText = document.getElementById("losses-text");
+var guessesLeft = document.getElementById("guesses-left");
+var guessedSoFar = document.getElementById("guessed-so-far");
+
+document.onkeyup = function(event) {
+  //computer choices a-z
+  var computerChoices = [
+    "a",
+    "b",
+    "c",
+    "d",
+    "e",
+    "f",
+    "g",
+    "h",
+    "i",
+    "j",
+    "k",
+    "l",
+    "m",
+    "n",
+    "o",
+    "p",
+    "q",
+    "r",
+    "s",
+    "t",
+    "u",
+    "v",
+    "w",
+    "x",
+    "y",
+    "z"
+  ];
+
+  // Determines which key user pressed
   var userGuess = event.key;
   console.log(userGuess);
 
@@ -13,9 +49,33 @@ document.onkeyup = function(event) {
   console.log(computerGuess);
 
   if (userGuess === computerGuess) {
+    won();
     console.log("cool");
   }
   if (userGuess !== computerGuess) {
-    console.log("WTF MATE");
+    if (numGuesses >= 1) {
+      numGuesses--;
+      console.log("WTF MATE");
+    }
+    if (numGuesses < 1) {
+      lose++;
+      resetGame();
+    }
   }
+
+  function won() {
+    wins++;
+    resetGame();
+  }
+
+  function resetGame() {
+    numGuesses = 9;
+    guessed = [];
+  }
+
+  //Displays guesses, wins and losses
+  guessedSoFar.textContent = userGuess;
+  winsText.textContent = wins;
+  lossesText.textContent = lose;
+  guessesLeft.textContent = numGuesses;
 };
